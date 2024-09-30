46abc0a86b749bde7c0825c34fb34c959c372c1d
YuliyMarkov <yuliymarkov01@gmail.com>
2024-09-30 18:55:57 +0500

    Finally rabotaet

--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -1,45 +1,74 @@
-import React from 'react';
-import Header from '../components/Header'; 
-import AppLabel from '../components/AppLabel';  
-import AppButton from '../components/AppButton';  
+import React, { useEffect, useState } from 'react';
+import { Header } from "../components/Header";
+import { AppLabel } from "../components/AppLabel";
+import { AppButton } from "../components/AppButton";
+
 const Welcome = () => {
+  const phoneRegex = /^\+?\d{1,4}?[\s-]?\(?\d{1,4}\)?[\s-]?\d{1,4}[\s-]?\d{1,4}[\s-]?\d{1,9}$/;
+  const nameRegex = /^[a-zA-Zа-яА-ЯёЁ]{1,20}$/;  
+  const [nameValue, setNameValue] = useState("");
+  const [phoneValue, setPhoneValue] = useState("");
+
+  const [checkBtn, setCheckBtn]=useState(true)
+  const [nameError, setNameError] = useState(false);
+  const [phoneError, setPhoneError] = useState(false);
+
+  const handleClick = () => {
+    if (!nameRegex.test(nameValue)) {
+      setNameError(true);
+    } else {
+      setNameError(false);
+    }
+    if (!phoneRegex.test(phoneValue)) {
+      setPhoneError(true);
+    } else {
+      setPhoneError(false);
+    }
+  };
+  useEffect(() => {
+    if(nameValue && phoneValue){
+      setCheckBtn(false)
+    } else{
+      setCheckBtn(true)
+    }
+  }, [nameValue, phoneValue]);
+
   return (
     <div className="container">
       <div className="wrapper">
         <div className="welcome">
-          <Header headerType="h1" headerText="Добро пожаловать в квиз от лучшего учебного центра"/>
+          <Header
+            headerType="h1"
+            headerText="Добро пожаловать в квиз от лучшего учебного центра"
+          />
           <form className="welcome__form">
-            <AppLabel/>
-            <AppLabel/>
-            {/* <label className="input-wrapper" htmlFor="username">
-              Ваше имя
-              <input
-                required
-                type="text"
-                name="username"
-                id="username"
-                placeholder="Ваш ответ"
-              />
-              <span id="error-message">
-                Введите номер в правильном формате например
-              </span>
-            </label>
-            <label className="input-wrapper" htmlFor="username">
-              Ваш номер
-              <input
-                required
-                type="tel"
-                name="phone"
-                id="phone"
-                placeholder="+998 9- --- -- -- "
-                pattern="^(?:\+998)?(?:\d{2})?(?:\d{7})$"
-              />
-              <span id="error-message">Введите номер в правильном формате</span>
-            </label> */}
-            {/* <button disabled type="submit" id="next-btn">
-              Далее
-            </button> */}
-            <AppButton/>
+            <AppLabel
+              labelText="Ваше имя"
+              errorText="Введите имя в правильном формате"
+              id="username"
+              isRequired
+              inputPlaceholder="Иван Иванов"
+              inputType="text"
+              labelChange={setNameValue}
+              labelValue={nameValue}
+              hasError={nameError}
+            />
+            <AppLabel 
+            labelText="Ваш номер"
+            errorText="Введите номер в правильном формате, например"
+            id="phone"
+            inputPlaceholder="+998 9- --- -- --"
+            inputType="tel"
+            isRequired={true}
+            labelChange={setPhoneValue}
+            labelValue={phoneValue}
+            hasError={phoneError}
+            />
+            <AppButton
+            buttonText="Далее"
+            isDisabled={checkBtn}
+            buttonClick={handleClick}
+            />
           </form>
         </div>
       </div>
